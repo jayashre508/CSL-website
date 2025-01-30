1bd9cbf9c7ddbdbcf4f175c51a8186d49aa2c46d
kay.jayashree@gmail.com <jayashree.k@medtrixhealthcare,com>
2025-01-30 18:26:58 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import VideoPage from "./Pages/videoPage";
+// import VideoPage from "./Pages/VideoPage";
 
 function App() {
   return (
     <BrowserRouter> 
       <Routes>
+        
         <Route path="/" element={<Home />} /> 
         <Route path="/videoPage" element={<VideoPage />} />
       </Routes>
